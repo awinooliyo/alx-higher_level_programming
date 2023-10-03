c97f2ddb6f44e6b85bef6d4d678f0001fc60ac3a
Erick Awino <erick.otieno89@gmail.com>
2023-10-03 19:47:24 +0300

    Task 6. Inventing is a combination of brains and materials. The more brains you use, the less material you need

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for j in range(0, 10):
     for k in range(j+1, 10):
         if j == 8 and k == 9:
-           print('89')
+            print('89')
         else:
             print('{}{}, '.format(j, k), end='')
