9dd87ee97f820397be37efd0ca45fb031afe79e1
Erick Awino <erick.otieno89@gmail.com>
2023-10-11 12:31:40 +0300

    Task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,10 +2,10 @@
 
 defquare_matrix_simple(matrix=[]):
     """
-    function that computes thequare value of all integers of a matrix
+    A function that computes the square values of all integers of a matrix
     """
     new_matrix = []
     for col in matrix:
         res = list(map(lambda x: x**2, col))
         new_matrix.append(res)
-    return new_matrix        
+    return new_matrix
