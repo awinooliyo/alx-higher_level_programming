e542fb2d95895a4bd9a06c7c989b1bb9796ba98e
Erick Awino <erick.otieno89@gmail.com>
2023-10-11 12:37:45 +0300

    Task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,8 +2,8 @@
 
 def search_replace(my_list, search, replace):
     """
-    A function that replaces all occurrences of 
-        an element by another in a new list.
+    A function that replaces all occurrences of
+    an element by another in a new list.
     """
 
     new_list = []
