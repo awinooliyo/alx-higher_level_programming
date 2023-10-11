a5883c201a1431ae8870a8baeabaef40291f8b12
Erick Awino <erick.otieno89@gmail.com>
2023-10-11 12:47:34 +0300

    Task 5

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def number_keys(a_dictionary):
+    """
+    A function that returns the number of keys in a dictionary.
+    """
+    n_keys = len(a_dictionary)
+    return n_keys
