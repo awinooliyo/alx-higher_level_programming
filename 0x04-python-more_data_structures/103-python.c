c62253129d989918cf06b57fecf280f18b0d5bce
Erick Awino <erick.otieno89@gmail.com>
2023-10-12 09:09:00 +0000

    Solving task 16 do in sandbox

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -2,11 +2,11 @@
 #include <Python.h>
 
 /**
-* print_python_bytes - Prints bytes information.
-*
-* @p Python Object
-* Return: no return
-*/
+ * print_python_bytes - Prints bytes information
+ *
+ * @p: Python Object
+ * Return: no return
+ */
 void print_python_bytes(PyObject *p)
 {
 	char *string;
@@ -15,37 +15,38 @@ void print_python_bytes(PyObject *p)
 	printf("[.] bytes object info\n");
 	if (!PyBytes_Check(p))
 	{
-		printf(" [Error] Invalid Bytes Object\n");
+		printf("  [ERROR] Invalid Bytes Object\n");
 		return;
 	}
 
 	size = ((PyVarObject *)(p))->ob_size;
-	string = ((PyBytesObject *)(p))->ob_sval;
+	string = ((PyBytesObject *)p)->ob_sval;
 
-	printf(" size: %ld\n", size);
-	printf(" trying string: %s\n", string);
+	printf("  size: %ld\n", size);
+	printf("  trying string: %s\n", string);
 
 	if (size >= 10)
 		limit = 10;
 	else
 		limit = size + 1;
 
-	printf(" first %ld bytes:", limit);
+	printf("  first %ld bytes:", limit);
 
 	for (i = 0; i < limit; i++)
 		if (string[i] >= 0)
 			printf(" %02x", string[i]);
 		else
 			printf(" %02x", 256 + string[i]);
+
 	printf("\n");
 }
 
 /**
-* print_python_list - Prints list information.
-*
-* @p: Python Object
-* Return: no return.
-*/
+ * print_python_list - Prints list information
+ *
+ * @p: Python Object
+ * Return: no return
+ */
 void print_python_list(PyObject *p)
 {
 	long int size, i;
@@ -62,7 +63,7 @@ void print_python_list(PyObject *p)
 	for (i = 0; i < size; i++)
 	{
 		obj = ((PyListObject *)p)->ob_item[i];
-		printf("Elemet %ld: %s\n", i, ((obj)->ob_type)->tp_name);
+		printf("Element %ld: %s\n", i, ((obj)->ob_type)->tp_name);
 		if (PyBytes_Check(obj))
 			print_python_bytes(obj);
 	}
