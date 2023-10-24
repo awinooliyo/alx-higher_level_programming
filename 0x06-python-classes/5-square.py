86448d8056498af942d25da4d6dbb7229d1e2523
Erick Awino <erick.otieno89@gmail.com>
2023-10-24 21:04:24 +0300

    Task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+"""define a square class"""
+
+
+class Square:
+    """
+    square class with a private attribure size
+    and checks data type
+    """
+    def __init__(self, size=0):
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("Size must be >= 0")
+
+        self.__size = size
+
+    @property
+    def size(self):
+        """retrieve the attribute"""
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """modify the attribute value"""
+
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """returns the current square area"""
+        return self.__size * self.__size
+
+    def my_print(self):
+        """print the square in #"""
+
+        if self.__size == 0:
+            print()
+
+        for i in range(self.__size):
+            print("#" * self.__size)
