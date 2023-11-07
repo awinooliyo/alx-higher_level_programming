08324f660c19621f2b447900bffb532822c13348
Erick Awino <erick.otieno89@gmail.com>
2023-11-07 14:26:10 +0300

    solution task 9

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -16,7 +16,7 @@ class Rectangle(BaseGeometry):
         self.__height = height
 
     def area(self):
-        """Returns area of the ractangle and overrides area() from superclass"""
+        """Returns area of a ractangle and overrides area() from superclass"""
         return self.__width * self.__height
 
     def __str__(self):
