340b8cfe41dba7cedfd4737f38ff13fbd276f289
Erick Awino <erick.otieno89@gmail.com>
2023-11-07 17:27:18 +0300

    solution for task 11

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Pascal's Triangle"""
+
+
+def pascal_triangle(n):
+    """
+    Represents the Pascal’s
+    triangle of size n
+    """
+
+    tri = []
+    if n <= 0:
+        return tri
+    if n == 0:
+        return [[1]]
+
+    tri = [[1]]
+    for i in range(n-1):
+        tri.append([a+b for a, b in zip([0] + tri[-1], tri[-1] + [0])])
+    return tri
