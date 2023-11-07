b8c84ddd3e5e6018e8d3b8ca9ea236c4b0797b28
Erick Awino <erick.otieno89@gmail.com>
2023-11-07 17:02:54 +0300

    solution for task 10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -5,18 +5,24 @@
 class Student:
     """Represent a student"""
 
-def __init__(self, first_name, last_name, age):
-    """Initializes a new student
-    """
-    self.first_name = first_name
-    self.last_name = last_name
-    self.age = age
-def to_json(self, attrs=None):
-    """Gets a dictionary representation of the Student
-    If attrs is a list of strings, represents only those attributes
-    included in the list
-    """
-    if (type(attrs) == list and
-            all(type(ele) == str for ele in attrs)):
-        return {k:getattr(self, k) for k in attrs if hasattr(self, k)}
-    return self.__dict__
+    def __init__(self, first_name, last_name, age):
+        """Initializes a new student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Gets a dictionary representation of the Student
+        If attrs is a list of strings, represents only those attributes
+        included in the list
+        """
+        if attrs is None:
+            return self.__dict__
+
+        else:
+            d = {}
+            for  attr in attrs:
+                if hasattr(self, attr):
+                    d[attr] = getattr(self, attr)
+            return d
