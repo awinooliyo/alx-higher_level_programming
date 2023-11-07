c9e4f2aa14f0d2e3a2d236d67bc48a4e773ea464
Erick Awino <erick.otieno89@gmail.com>
2023-11-07 17:33:43 +0300

    solution for task 9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -5,14 +5,12 @@
 class Student:
     """Represent a student"""
 
-
     def __init__(self, first_name, last_name, age):
         """Initializes a new student
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-
 
     def to_json(self):
         """Gets a dictionary representation of the student"""
