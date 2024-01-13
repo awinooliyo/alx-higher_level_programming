c36714b47cc12614f01446f22931c0b95ab18d47
Erick Awino <erick.otieno89@gmail.com>
2024-01-13 22:28:41 +0300

    Task no 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """
-Module that connects a python script to a database
+ This is a script that takes in the name of a state
+ as an argument and lists all cities of that state,
+ using the database hbtn_0e_4_usa
 """
 
 if __name__ == "__main__":
