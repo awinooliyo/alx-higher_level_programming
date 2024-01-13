91c26034b33400604801bd410388c2fd0e2f7349
Erick Awino <erick.otieno89@gmail.com>
2024-01-13 20:42:01 +0300

    Task no 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 """
-Module that connects python script to a database
+a script that lists all states with a name starting with
+(upper N) from the database
 """
 if __name__ == "__main__":
     import MySQLdb
@@ -20,7 +21,7 @@ if __name__ == "__main__":
     my_cursor = my_db.cursor()
     my_cursor.execute(
         """SELECT * FROM states WHERE name LIKE
-        BINARY 'N%'ORDER BY states.id ASC
+        BINARY 'N%' ORDER BY states.id ASC
         """
         )
 
