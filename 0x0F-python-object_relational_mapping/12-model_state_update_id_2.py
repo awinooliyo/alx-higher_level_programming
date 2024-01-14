7de1faab022c9e836411d5d400d98121af151e00
Erick Awino <erick.otieno89@gmail.com>
2024-01-14 19:13:11 +0300

    Task 12

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
 """
-A script that changes the name of a State object from the database hbtn_0e_6_usa
+A script that changes the name of a
+State object from the database hbtn_0e_6_usa
 """
 from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State # W: 'model_state.Base' imported but unused
+from model_state import Base, State
 
 if __name__ == "__main__":
 
