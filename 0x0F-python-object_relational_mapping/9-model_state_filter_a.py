43f7337d4974ee47f64e2aa2c191653c124b20a1
Erick Awino <erick.otieno89@gmail.com>
2024-01-14 21:04:00 +0300

    Task 9

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
 
+"""
+A script that lists all State objects that
+contain the letter a from
+the database hbtn_0e_6_usa
+"""
+
 from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
+
     engine = create_engine(
         'mysql+mysqldb://{}:{}@localhost/{}'
         .format(argv[1], argv[2], argv[3]),
