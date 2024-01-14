04678398699e579a762cdc745b28173e0d7cd02a
Erick Awino <erick.otieno89@gmail.com>
2024-01-14 17:44:36 +0300

    Model State

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+Module that connects to the database
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    """Defining the State class"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
