b8ee1ac32e50488b7adc8e4f7ab18a0a4a9df754
Erick Awino <erick.otieno89@gmail.com>
2024-01-29 19:06:25 +0300

    Task three

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""
-    script that takes in a URL, sends a request to the URL
-    and displays the body of the response (decoded in utf-8).
-"""
+""" Sending a request using urllib"""
 
 
 import sys
